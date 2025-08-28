d53b1999914c6239dbe53426233513b71f67b6fd
Anwitha R <anwitha583@gmail.com>
2025-08-28 20:52:39 +0530

    Add files via upload

---
+++ Calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_appApp.swift
+//  Calculator app
+//
+//  Created by Anwitha R on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
